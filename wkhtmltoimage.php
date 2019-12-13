7eb0355e1901cce782333350b357b3e15d254f76
clone <ivan@mod24.com>
2019-12-13 14:55:17 +0100

    Update paths

--- wkhtmltoimage.php
+++ wkhtmltoimage.php
@@ -3,6 +3,7 @@ namespace Wkhtmltox;
 
 class Wkhtmltoimage
 {
-    const BIONICAMD64 = __DIR__ . '/bin/wkhtmltoimage-amd64';
+    const BIONICAMD6416 = __DIR__ . '/bin/16/wkhtmltoimage-amd64';
+    const BIONICAMD6418 = __DIR__ . '/bin/18/wkhtmltoimage-amd64';
 }
 
