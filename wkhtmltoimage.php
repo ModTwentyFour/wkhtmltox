d5f7b311a3f3923a09bbd6352757110ad664c41e
clone <ivan@mod24.com>
2019-12-14 13:41:15 +0100

    Restructure bin directory

--- wkhtmltoimage.php
+++ wkhtmltoimage.php
@@ -3,7 +3,7 @@ namespace Wkhtmltox;
 
 class Wkhtmltoimage
 {
-    const BIONICAMD6416 = __DIR__ . '/bin/16/wkhtmltoimage-amd64';
-    const BIONICAMD6418 = __DIR__ . '/bin/18/wkhtmltoimage-amd64';
+    const BIONICAMD6416 = __DIR__ . '/bin/wkhtmltoimage-amd64-16';
+    const BIONICAMD6418 = __DIR__ . '/bin/wkhtmltoimage-amd64-18';
 }
 
--- wkhtmltopdf.php
+++ wkhtmltopdf.php
@@ -3,7 +3,7 @@ namespace Wkhtmltox;
 
 class Wkhtmltopdf
 {
-    const BIONICAMD6416 = __DIR__ . '/bin/16/wkhtmltopdf-amd64';
-    const BIONICAMD6418 = __DIR__ . '/bin/18/wkhtmltopdf-amd64';
+    const BIONICAMD6416 = __DIR__ . '/bin/wkhtmltopdf-amd64-16';
+    const BIONICAMD6418 = __DIR__ . '/bin/wkhtmltopdf-amd64-18';
 }
 
